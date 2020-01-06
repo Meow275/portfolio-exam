7564a634276571bae5f62035a6e4aac826e674c3
Meow275 <ruta@lemme.pro>
2020-01-06 17:51:06 +0200

    Prompt opening mailto application

--- about.js
+++ about.js
@@ -1,3 +1,10 @@
 function sendContact() {
+     console.log("hello", document.getElementsByTagName('form')[0]);
+    var formData = new FormData(document.getElementsByTagName('form')[0]);
+    var lastName = formData.get('lastName');
+    var firstName = formData.get('firstName');
+    var email = formData.get('email');
+    var message = formData.get('message');
+    window.open('mailto:' + email + '?subject=request from ' + firstName + ' ' + lastName +'&body=' + message);
 
 }
